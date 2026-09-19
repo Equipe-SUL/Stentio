058072fac1c56428d8fbd5377c9fd48a2bb2957d
yCels <celso5mf@gmail.com>
2026-09-19 15:40:14 -0300

    fetat(Dto): adiciona Dto Request e Responde para CategoriaProjeto

---
+++ Core/src/main/java/com/example/stentio/core/dto/CategoriaProjetoRequest.java
@@ -0,0 +1,11 @@
+package com.example.stentio.core.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CategoriaProjetoRequest (
+
+    @NotBlank
+    @Size(max = 40, message = "Nome deve ter no máximo 40 caracteres")
+    String nome
+){}
