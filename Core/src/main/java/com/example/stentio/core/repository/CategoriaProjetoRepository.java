d355eef9d0e44101252ce4a71dab8d35164a58c0
yCels <celso5mf@gmail.com>
2026-09-19 15:39:22 -0300

    feat(repository): adiciona CategoriaProjetorepository

---
+++ Core/src/main/java/com/example/stentio/core/repository/CategoriaProjetoRepository.java
@@ -0,0 +1,16 @@
+package com.example.stentio.core.repository;
+
+import com.example.stentio.core.model.CategoriaProjeto;
+import com.example.stentio.core.model.Idioma;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CategoriaProjetoRepository extends JpaRepository<CategoriaProjeto, UUID> {
+    boolean existsByNome(String nome);
+    Page<CategoriaProjeto> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+
+
+}
