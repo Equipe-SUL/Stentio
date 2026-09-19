c99cebe2005bb195f0f8e3ec23bc9fcd76ad7764
yCels <celso5mf@gmail.com>
2026-09-19 00:44:29 -0300

    feat(repository): adiciona IdiomaRepository

--- Core/src/main/java/com/example/stentio/core/repository/IdiomaRepository.java
+++ Core/src/main/java/com/example/stentio/core/repository/IdiomaRepository.java
@@ -1,9 +1,15 @@
 package com.example.stentio.core.repository;
 
 import com.example.stentio.core.model.Idioma;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface IdiomaRepository extends JpaRepository<Idioma, UUID> {
+    boolean existsByCodigoIso(String codigoIso);
+    //bussca idiomas se nome tem o texto informado, ignorando maiúsculas/minúsculas,
+    Page<Idioma> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+
 }
